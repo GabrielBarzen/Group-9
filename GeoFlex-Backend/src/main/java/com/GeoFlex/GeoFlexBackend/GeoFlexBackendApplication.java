8e8f2dcac303b334cfd5f2739cb655f75ffd7376
Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>
2022-10-03 14:49:12 +0200

    Initial backend commit

---
+++ GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/GeoFlexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GeoFlex.GeoFlexBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoFlexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoFlexBackendApplication.class, args);
+	}
+
+}
