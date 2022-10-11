e961acd2f63f7dd952aa520470df7195d6a1fb73
Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>
2022-10-11 16:38:05 +0200

    Update testMain.java

--- GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/Test/testMain.java
+++ GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/Test/testMain.java
@@ -1,16 +1,20 @@
 package com.GeoFlex.GeoFlexBackend.Test;
 
 import com.GeoFlex.GeoFlexBackend.DatabaseAccess.DatabaseConnection;
+import com.GeoFlex.GeoFlexBackend.DatabaseAccess.Procedures;
 import com.GeoFlex.GeoFlexBackend.Process.VideoManager;
 
 import java.io.IOException;
 
 public class testMain {
     public static void main(String[] args) throws IOException {
-        //DatabaseConnection dc = new DatabaseConnection();
-        //dc.queryTest();
+        DatabaseConnection dc = new DatabaseConnection();
+        //Pass the DataBaseConnection objcet to the constructor.
+        Procedures procedures = new Procedures(dc);
+        //procedures.queryTest();
+        procedures.procedureTest();
 
-        VideoManager vm = new VideoManager();
-        vm.convertToWebm();
+        //VideoManager vm = new VideoManager();
+        //vm.convertToWebm();
     }
 }
