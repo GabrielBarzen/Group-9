c7944bdf47486ac386433758c986dbf7b3840ebc
Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>
2022-10-11 15:02:03 +0200

    FFMPEG support
    - Implemented FFMPEG support to process videos and images.
    
    - Added some test videos.

--- GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/Test/testMain.java
+++ GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/Test/testMain.java
@@ -1,10 +1,16 @@
 package com.GeoFlex.GeoFlexBackend.Test;
 
 import com.GeoFlex.GeoFlexBackend.DatabaseAccess.DatabaseConnection;
+import com.GeoFlex.GeoFlexBackend.Process.VideoManager;
+
+import java.io.IOException;
 
 public class testMain {
-    public static void main(String[] args) {
-        DatabaseConnection dc = new DatabaseConnection();
-        dc.queryTest();
+    public static void main(String[] args) throws IOException {
+        //DatabaseConnection dc = new DatabaseConnection();
+        //dc.queryTest();
+
+        VideoManager vm = new VideoManager();
+        vm.convertToWebm();
     }
 }
