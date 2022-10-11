dc997f191795c0efc0c832639f4e244e35540d9b
Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>
2022-10-11 16:34:42 +0200

    Test code for stored procedures.

--- GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/DatabaseAccess/DatabaseConnection.java
+++ GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/DatabaseAccess/DatabaseConnection.java
@@ -13,28 +13,12 @@ public class DatabaseConnection {
      */
     public DatabaseConnection(){
         try {
+            //Connect to database.
             conn = DriverManager.getConnection("jdbc:mariadb://gabnet.se:33306/GeoFlex?user="+username+"&password="+password);
             System.out.println("Connected to database.");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    /**
-     * Test function.
-     */
-    public void queryTest(){
-        try(PreparedStatement statement = conn.prepareStatement("SELECT * FROM route")) {
-            ResultSet rs = statement.executeQuery();
-            while(rs.next()){
-                System.out.println(rs.getString(1));
-                System.out.println(rs.getString(2));
-                System.out.println(rs.getString(3));
-                System.out.println(rs.getString(4));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } ;
     }
 
     /**
