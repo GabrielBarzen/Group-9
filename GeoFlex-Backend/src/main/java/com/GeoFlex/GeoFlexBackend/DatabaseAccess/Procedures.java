646de9eed540682fd97fc79fe385e1e3748fe70a
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2022-10-14 11:52:35 +0200

    getRouteFromDb

--- GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/DatabaseAccess/Procedures.java
+++ GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/DatabaseAccess/Procedures.java
@@ -1,9 +1,14 @@
 package com.GeoFlex.GeoFlexBackend.DatabaseAccess;
 
+import com.GeoFlex.GeoFlexBackend.PoJo.Content;
+import com.GeoFlex.GeoFlexBackend.PoJo.Location;
 import com.GeoFlex.GeoFlexBackend.PoJo.Root;
 import com.GeoFlex.GeoFlexBackend.PoJo.Route;
+import com.google.gson.Gson;
+import org.checkerframework.checker.units.qual.C;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * This class handles all the stored procedures in the database.
@@ -113,6 +118,10 @@ public class Procedures {
         }
     }
 
+    public static void main(String[] args) {
+        Procedures proc = new Procedures(new DatabaseConnection());
+        proc.getRouteFromDatabase(1,0);
+    }
     public void getRouteFromDatabase(int routeId, int routeCode){
         try {
             CallableStatement cs = dc.getConnection().prepareCall("{CALL sp_get_full_route_no_imgvideo(?, ?)}");
@@ -120,7 +129,47 @@ public class Procedures {
             cs.setInt(2, routeCode);
             cs.executeQuery();
             ResultSet res = cs.getResultSet();
+
+            Root r = new Root();
+            boolean first = true;
+            String currentLocationId = "0";
+            Location currentLocation = new Location();
             while(res.next()){
+                if (first) {
+                    r.route = new Route();
+                    r.route.id = res.getString(1);
+                    r.route.title = res.getString(2);
+                    r.route.description = res.getString(3);
+                    r.route.code = res.getString(4);
+                    r.route.type = res.getString(5);
+                    r.route.location = new ArrayList<>();
+                }
+                if (!currentLocationId.equals(res.getString(7))) {
+                    System.out.println("current id : " + currentLocationId);
+                    System.out.println("Current res id : " + res.getString(7));
+                    if (!first) {
+                        r.route.location.add(currentLocation);
+                    } else {
+                        first = false;
+                    }
+                    currentLocationId = res.getString(7);
+                    currentLocation = new Location();
+                    currentLocation.content = new ArrayList<>();
+                    currentLocation.id = currentLocationId;
+                    currentLocation.name = res.getString(8);
+                    currentLocation.text_info = res.getString(9);
+                }
+                Content content = new Content();
+                content.id = res.getString(10);
+                content.answer = res.getString(11);
+                content.correct = res.getBoolean(12);
+                currentLocation.content.add(content);
+            }
+            r.route.location.add(currentLocation);
+            Gson gson = new Gson();
+            System.out.println(gson.toJson(r));
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
