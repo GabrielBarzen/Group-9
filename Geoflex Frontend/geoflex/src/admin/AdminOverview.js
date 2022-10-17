70877a337ecabc0c66ec902d465f18b48231f4de
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-10-17 14:09:25 +0200

    Update AdminOverview.js

--- Geoflex Frontend/geoflex/src/admin/AdminOverview.js
+++ Geoflex Frontend/geoflex/src/admin/AdminOverview.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import AdminAddNew from './AdminAddNew';
 
 export default function AdminOverview() {
+    const quiz = (()=>{
+        axios
+    });
     return (
         <section>
             <h2>Översikt</h2>
             <button className="quiz">Runes Quiz</button>
             <button className="info">Runes Rundtur</button>
             <button className="mixed">Runes Roliga Runda</button>
-            <button id="add-new">Lägg till</button>
+            <Link to='/admin/new/'>
+                <button id="add-new">Lägg till</button>
+            </Link>
+            <AdminAddNew />
         </section>
     )
 }
