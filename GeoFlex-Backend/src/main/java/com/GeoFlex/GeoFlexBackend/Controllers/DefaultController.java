8e6c7ae301dbac29aa4c7a0dc97c79a0cd225320
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2022-10-24 11:33:15 +0200

    got react to serve manually

---
+++ GeoFlex-Backend/src/main/java/com/GeoFlex/GeoFlexBackend/Controllers/DefaultController.java
@@ -0,0 +1,14 @@
+package com.GeoFlex.GeoFlexBackend.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class DefaultController {
+    @GetMapping("/")
+    public String index(){
+        System.out.println("attempt get index.html");
+        return "index.html";
+    }
+}
