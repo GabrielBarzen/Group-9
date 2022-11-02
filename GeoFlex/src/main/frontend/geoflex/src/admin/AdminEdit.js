3f2a6de7d959d0f056d251dcb87e36028761fcf5
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-11-02 13:56:21 +0100

    Update AdminEdit.js

--- GeoFlex/src/main/frontend/geoflex/src/admin/AdminEdit.js
+++ GeoFlex/src/main/frontend/geoflex/src/admin/AdminEdit.js
@@ -9,7 +9,7 @@ export default function AdminEdit() {
     const data = location.state.data
 
     const [routeLocationsData, setEditLocations] = useState([])
-    const data2 = { "route": { "location": [{ "name": "1", "text_info": "Replace me", "id": "179", "location_index": "1", "last_location": "false" }, { "name": "2", "text_info": "Replace me", "id": "180", "location_index": "2", "last_location": "false" }, { "name": "3", "text_info": "Replace me", "id": "181", "location_index": "3", "last_location": "false" }, { "name": "4", "text_info": "Replace me", "id": "182", "location_index": "4", "last_location": "false" }, { "name": "5", "text_info": "Replace me", "id": "183", "location_index": "5", "last_location": "false" }, { "name": "6", "text_info": "Replace me", "id": "184", "last_location": "true" }], "locations": 0 } };
+    //const data2 = { "route": { "location": [{ "name": "1", "text_info": "Replace me", "id": "179", "location_index": "1", "last_location": "false" }, { "name": "2", "text_info": "Replace me", "id": "180", "location_index": "2", "last_location": "false" }, { "name": "3", "text_info": "Replace me", "id": "181", "location_index": "3", "last_location": "false" }, { "name": "4", "text_info": "Replace me", "id": "182", "location_index": "4", "last_location": "false" }, { "name": "5", "text_info": "Replace me", "id": "183", "location_index": "5", "last_location": "false" }, { "name": "6", "text_info": "Replace me", "id": "184", "last_location": "true" }], "locations": 0 } };
 
 
     const [updateTour, setUpdateTour] = useState();
@@ -34,7 +34,7 @@ export default function AdminEdit() {
             })
             .catch(function (error) {
                 console.log(error.response.data);
-                setEditLocations(data2)
+                //setEditLocations(data2)
             });
     })
 
@@ -87,7 +87,7 @@ export default function AdminEdit() {
                 <option value="INFO">Inforunda</option>
             </select>
             <ul className="">
-                {[...data2.route.location].map(location => <Location key={location.id} data={location} />)}
+                {[...routeLocationsData.route.location].map(location => <Location key={location.id} data={location} />)}
 
             </ul>
             <button onClick={event => handleSave(event)}>Spara</button>
