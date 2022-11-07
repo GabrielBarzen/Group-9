0d4f3c26595f0d2730c314f97ede61092a9981d1
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-11-07 20:02:44 +0100

    fix last location partial

--- GeoFlex/src/main/frontend/geoflex/src/admin/components/Location.js
+++ GeoFlex/src/main/frontend/geoflex/src/admin/components/Location.js
@@ -6,14 +6,14 @@ export default function Location(props) {
     const [stateObject, setObjectState] = useState({
         firstKey: '',
         secondKey: '',
-      });
-      
-      setObjectState((prevState) => ({
+    });
+
+    setObjectState((prevState) => ({
         ...prevState,
         secondKey: 'value',
-      }));
+    }));
     return (
-        <>
+        <>{!props.data.last_location ? (
             <li className='row card-panel'>
 
                 <i className="material-icons col s1">place</i>
@@ -23,8 +23,20 @@ export default function Location(props) {
                 <button onClick={() => { props.deleteLocation(props.data.id) }}>
                     Ta bort
                 </button>
-                <button id={props.data.id} data_location_index={props.data.location_index} onClick={event => { props.swapLocationsUp(event, props.data.id) }}>Flytta upp</button>
+                <button
+                    id={props.data.id}
+                    data_location_index={props.data.location_index}
+                    onClick={event => { props.swapLocationsUp(event, props.data.id) }}>
+                    Flytta upp
+                </button>
             </li>
+        ) : (
+            <li className='row green lighten-2 card-panel'>
+                <i className="material-icons col s1">cake</i>
+                <span className='col s9'>
+                    {props.data.name}
+                </span>
+            </li>)}
         </>
     )
 }
