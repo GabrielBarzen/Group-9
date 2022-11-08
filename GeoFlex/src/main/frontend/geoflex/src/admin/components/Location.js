bcea0bb820fb8808bf3ad0426116fcfc32f61493
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-11-08 09:08:30 +0100

    Update Location.js
    ett försök att få rätt på knapparna i locationslistan.
    Lite longshot, vet faktiskt inte om det här kommer fungera :D

--- GeoFlex/src/main/frontend/geoflex/src/admin/components/Location.js
+++ GeoFlex/src/main/frontend/geoflex/src/admin/components/Location.js
@@ -2,36 +2,87 @@ import React, { useState } from 'react'
 
 
 export default function Location(props) {
-/*
-    const [stateObject, setObjectState] = useState({
-        firstKey: '',
-        secondKey: '',
-    });    
+    /*
+        const [stateObject, setObjectState] = useState({
+            firstKey: '',
+            secondKey: '',
+        });    
+    
+        setObjectState((prevState) => ({
+            ...prevState, 
+            secondKey: 'value',
+        }));
+    */
+    function renderLocation() {
+        props.data.forEach(element => {
+            if (element.location_index === "1") {
+                return (
+                    <li className='row card-panel'>
 
-    setObjectState((prevState) => ({
-        ...prevState, 
-        secondKey: 'value',
-    }));
-*/
+                        <i className="material-icons col s1">place</i>
+                        <span className='col s9'>
+                            {props.data.name}
+                        </span>
+                        <button onClick={() => { props.deleteLocation(props.data.id) }}>
+                            Ta bort
+                        </button>
+                        <button onClick={() => { props.swapLocationsDown(props.data.id) }}>
+                            Flytta ner
+                        </button>
+                    </li>
+                )
+            } else if (element.location_index !== element.location_index.length - 2) {
+                return (
+                    <li className='row card-panel'>
+
+                        <i className="material-icons col s1">place</i>
+                        <span className='col s9'>
+                            {props.data.name}
+                        </span>
+                        <button onClick={() => { props.deleteLocation(props.data.id) }}>
+                            Ta bort
+                        </button>
+                        <button onClick={() => { props.swapLocationsUp(props.data.id) }}>
+                            Flytta upp
+                        </button>
+                        <button onClick={() => { props.swapLocationsDown(props.data.id) }}>
+                            Flytta ner
+                        </button>
+                    </li>
+                )
+            } else if (element.location_index === element.location_index.length - 1) {
+                return (
+                    <li className='row card-panel'>
+
+                        <i className="material-icons col s1">place</i>
+                        <span className='col s9'>
+                            {props.data.name}
+                        </span>
+                        <button onClick={() => { props.deleteLocation(props.data.id) }}>
+                            Ta bort
+                        </button>
+                        <button onClick={() => { props.swapLocationsUp(props.data.id) }}>
+                            Flytta upp
+                        </button>
+                    </li>
+                )
+            } else if (element.last_location === "true") {
+                return (
+                    <li className='row card-panel'>
+
+                        <i className="material-icons col s1">place</i>
+                        <span className='col s9'>
+                            {props.data.name}
+                        </span>
+                    </li>
+                )
+            }
+        });
+    }
 
     return (
         <>
-            <li className='row card-panel'>
-
-                <i className="material-icons col s1">place</i>
-                <span className='col s9'>
-                    {props.data.name}
-                </span>
-                <button onClick={() => { props.deleteLocation(props.data.id) }}>
-                    Ta bort
-                </button>
-                <button onClick={() => { props.swapLocationsUp(props.data.id) }}>
-                    Flytta upp
-                </button>
-                <button onClick={() => { props.swapLocationsDown(props.data.id) }}>
-                    Flytta ner
-                </button>
-            </li>
+            {displayLocation()}
         </>
     )
 }
