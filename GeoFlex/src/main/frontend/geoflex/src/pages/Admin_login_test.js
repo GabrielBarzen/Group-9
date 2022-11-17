f2312dc33bac1863302c483afad5f1f415ac2326
Rasmus Appelgren <rasmus.appelgren@gmail.com>
2022-11-17 15:33:15 +0100

    Update admin login test with design

--- GeoFlex/src/main/frontend/geoflex/src/pages/Admin_login_test.js
+++ GeoFlex/src/main/frontend/geoflex/src/pages/Admin_login_test.js
@@ -1,22 +1,39 @@
 import Logo from "../shared/Logo"
 import Login from "../shared/Login"
+import Button from "../shared/Button"
 
 
 
 export default function AdminLogin() {
-    return (
-        <div className="container">
-            <div className="row">
-                <Logo />
-            </div>
-            <div className="row">
-                <div className="white z-depth-2 container-css col s10"> 
-                    <div className="row">
-                        <Login />
+    return (<>
+    <div className="row">
+        <div className="container white container-css">
+            <div class="row center-align">
+                <h5>Logga in</h5>
+                <form class="col s12">
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">account_box</i>
+                            <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                            <label for="icon_prefix2">Användarnamn</label>
+                        </div>
                     </div>
-            </div>
-            </div> 
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">lock</i>
+                            <input id="password" type="password" class="validate"/>
+                            <label for="password">Lösenord</label>
+                        </div>
+                    </div>
+                </form>
+            </div>          
         </div>
-
+    </div>
+        <div className="row">
+            <div className="col s12 offset-s1"> 
+            <Button text="Logga in" css="col s10" icon={<i className="small material-icons right">arrow_forward</i>}/>
+            </div>
+        </div>
+        </>
     )
 }
