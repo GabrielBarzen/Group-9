1d585b1f8734c81d19080ba9ea4831e99ae5f8b7
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2022-11-21 15:18:10 +0100

    Assign pojos

---
+++ GeoFlex/src/main/java/com/GeoFlex/GeoFlexBackend/PoJo/ModeratorAssign/Route.java
@@ -0,0 +1,17 @@
+package com.GeoFlex.GeoFlexBackend.PoJo.ModeratorAssign;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Route {
+
+    @SerializedName("assign")
+    @Expose
+    public String assign;
+    @SerializedName("un-assign")
+    @Expose
+    public String unAssign;
+
+}
