9e874d0f8686027868f44c80cddfeae6ed44fd48
Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>
2022-11-21 12:40:31 +0100

    Functions for backend connection

--- GeoFlex/src/main/frontend/geoflex/src/admin/AdminModeratorEdit.js
+++ GeoFlex/src/main/frontend/geoflex/src/admin/AdminModeratorEdit.js
@@ -1,53 +1,95 @@
 import React, { useEffect, useState } from 'react';
 import AdminModeratorOverview from './components/AdminModeratorOverview';
 import M from 'materialize-css';
+import axios from "axios";
 
 
 export default function AdminModeratorEdit() {
     const [moderators, setModerators] = useState([
         {
-        "name":"NewModerator",
-        "user-id":69
+            "name": "NewModerator",
+            "user-id": 69
         },
         {
-        "name":"Max",
-        "user-id":78
+            "name": "Max",
+            "user-id": 78
         },
         {
-        "name":"Jack",
-        "user-id":79
+            "name": "Jack",
+            "user-id": 79
         },
         {
-        "name":"Lux",
-        "user-id":80
+            "name": "Lux",
+            "user-id": 80
         },
         {
-        "name":"Sivir",
-        "user-id":81
+            "name": "Sivir",
+            "user-id": 81
         }
-        ]
-        );
+    ]
+    );
 
-    useEffect(() =>{
+    useEffect(() => {
         M.AutoInit();
-        moderator();
+        getModeratorList();
     });
 
-    function moderator(){
-        axios
+    function getModeratorList() {
+        var config = {
+            method: 'get',
+            url: '/admin/moderators',
+            headers: {}
+        };
 
-        setModerators(response.data.moderators)
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                setModerators(response.data.moderators)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    function createModerator() {
+        var data = JSON.stringify(
+            {
+                "create-moderator":
+                {
+                    "name": "Sätt värde från input",
+                    "email": "Sätt värde från input",
+                    "password": "Sätt värde från input"
+                }
+            }
+        );
+
+        var config = {
+            method: 'post',
+            url: '/admin/create/moderator',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            data: data
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     return (<>
-    <div className='white'>
-    <p>Översikt på moderatorer</p>
-    <ul className='collapsible'>
-        {[...moderators].map((moderator) => (
-            <AdminModeratorOverview key={moderator['user-id']} data={moderator} />
-        ))}
-    </ul>
-    </div>
+        <div className='white'>
+            <p>Översikt på moderatorer</p>
+            <ul className='collapsible'>
+                {[...moderators].map((moderator) => (
+                    <AdminModeratorOverview key={moderator['user-id']} data={moderator} />
+                ))}
+            </ul>
+        </div>
     </>
     )
 }
