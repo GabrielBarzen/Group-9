b37f3139a01b2c009c8f0cd6c3944e854cb5f0f3
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-11-22 10:12:03 +0100

    fixade moderator ID

--- GeoFlex/src/main/frontend/geoflex/src/admin/components/AdminModeratorOverview.js
+++ GeoFlex/src/main/frontend/geoflex/src/admin/components/AdminModeratorOverview.js
@@ -6,22 +6,20 @@ import M from 'materialize-css';
 export default function AdminModeratorOverview(props) {
     const [selectItems, setSelectItems] = useState([]);
     const routeData = props.moderatorRoutesData;
-    console.log("routeData")
-    console.log(routeData)
+
 
     const handleSelectOptions = () => {
-        console.log("HÄR")
+        
         const dummyData = [{ "title": "Test Quiz", "description": "This quiz is for testing purposes.", "type": "QUIZ", "id": "1", "code": "572748", "locations": 3 }, { "title": "Test Info", "description": "This info for testing purposes.", "type": "INFO", "id": "2", "code": "184471", "locations": 3 }, { "title": "Test 2", "description": "More testing tests ", "type": "INFO", "id": "4", "code": "295052", "locations": 0 }, { "title": "Num Location Test1", "description": "test, remove", "type": "INFO", "id": "5", "code": "447827", "locations": 0 }, { "title": "Num Location Test2", "description": "test, remove", "type": "INFO", "id": "6", "code": "625158", "locations": 3 }, { "title": "Num Location Test3", "description": "test, remove", "type": "INFO", "id": "7", "code": "782310", "locations": 4 }, { "title": "Test Quiz2E", "description": "This quiz is for testing purposes.", "type": "QUIZ", "id": "8", "code": "538027", "locations": 6 }, { "title": "Test Quizz", "description": "This quiz is for testing purposes.", "type": "QUIZ", "id": "10", "code": "983850", "locations": 6 }];
 
 
         let allRoutes = props.allRoutesData;
         let routes = props.moderatorRoutesData;
-        console.log("DENNA" + allRoutes);
-        console.log(routes);
+
 
         var leftUsers = allRoutes.filter(u => routeData.findIndex(lu => lu.id === u.id) === -1);
 
-        console.log(leftUsers);
+
         /*
                 let allRoutes = props.allRoutesData;
                 let availableRoutes = routeData.filter(val => !allRoutes.includes(val));
@@ -38,7 +36,7 @@ export default function AdminModeratorOverview(props) {
 
     useEffect(() => {
         M.AutoInit();
-        props.getRouteForUser(props.data.id)
+        props.getRouteForUser(props.data["user-id"])
     }, []);
 
     return (<>
