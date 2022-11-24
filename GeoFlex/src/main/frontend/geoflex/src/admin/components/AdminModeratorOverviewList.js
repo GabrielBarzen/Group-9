4ee4dec95d115d1484e1a806f542d01977f9fc20
Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>
2022-11-24 22:06:15 +0100

    Moderators to left side and delete function

--- GeoFlex/src/main/frontend/geoflex/src/admin/components/AdminModeratorOverviewList.js
+++ GeoFlex/src/main/frontend/geoflex/src/admin/components/AdminModeratorOverviewList.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import M from 'materialize-css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function AdminModeratorOverviewList(props) {
 
@@ -11,12 +12,31 @@ export default function AdminModeratorOverviewList(props) {
         console.log(props.data.name)
     }, []);
 
+    /**
+ * Deletes a moderator.
+ */
+    function handleDeleteModerator() {
+        var config = {
+            method: 'post',
+            url: '/admin/delete/moderator?user-id=' + props.data["user-id"],
+            headers: {}
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     return (<>
 
         <li className='collection-item row'>
 
             <i className="material-icons col s1">person</i>
-            <Link className="col s9 offset-s1" style={{ cursor: 'pointer', 'fontSize': '1rem', 'color': 'black' }} to={url} state={{ data: props.data }}>
+            <Link className="left" style={{ cursor: 'pointer', 'fontSize': '1rem', 'color': 'black' }} to={url} state={{ data: props.data }}>
                 {props.data.name}
             </Link>
             <span className='col s1 right' style={{ cursor: 'pointer', 'fontSize': '2rem' }}><i className='material-icons right black-text' id={props.data["user-id"]} onClick={() => { props.deleteItem(props.data["user-id"]) }}>delete_forever</i></span>
