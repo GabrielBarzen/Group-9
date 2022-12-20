ed54fbb4c47f327118b69bb69ade7c647c246e8e
Rasmus Appelgren <rasmus.appelgren@gmail.com>
2022-12-20 13:48:57 +0100

    Map, markers etc in place
    Co-Authored-By: Linus Ingvarsson <81238060+linke-droid@users.noreply.github.com>

--- GeoFlex/src/main/frontend/geoflex/src/game/TestMap.js
+++ GeoFlex/src/main/frontend/geoflex/src/game/TestMap.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import visitorIcon from "./Constants";
+import CurrentPosIcon from "./Map-Pin.png"
 
 
 
@@ -17,20 +18,28 @@ const Maps = () => {
       map.locate().on("locationfound", function (e) {
         setPosition(e.latlng);
         map.flyTo(e.latlng, map.getZoom());
-        console.log(e.latlng)
       });
     }, []);
+    var L = window.L;
+    let posIcon = L.icon({
+        iconUrl: CurrentPosIcon,
+        iconRetinaUrl: CurrentPosIcon,
+        iconAnchor: [5, 55],
+        popupAnchor: [10, -44],
+        iconSize: [35, 40],
+      });
 
     return position === null ? null : (
-      <Marker position={position} icon={visitorIcon}>
+      <Marker position={position} icon={posIcon} autoClose={false}>
         <Popup>Här är du</Popup>
       </Marker>
     );
   }
+
   function QuestionMarker() {
     /*
     Här måste vi läsa in koordinater från API till frågan.
-    Koordinaterna sätts in i position
+    Koordinaterna sätts in i position.
     */
     return (
         <Marker position={[55.681922, 12.999577]} icon={visitorIcon}>
@@ -39,12 +48,13 @@ const Maps = () => {
     )
   }
 
+
   return (
     <MapContainer
       center={[55.604981, 13.003822000000014]}
       zoom={13}
       scrollWheelZoom
-      style={{ height: "100vh" }}
+      style={{ height: "50vh" }}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -54,6 +64,7 @@ const Maps = () => {
       <LocationMarker />
       <QuestionMarker />
     </MapContainer>
+    
   );
 };
 
