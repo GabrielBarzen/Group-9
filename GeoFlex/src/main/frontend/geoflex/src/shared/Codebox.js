c26d72c5c87991fb387d63867c3b096245f536cc
Rasmus Appelgren <rasmus.appelgren@gmail.com>
2023-01-09 13:22:05 +0100

    Codebox design
    Co-Authored-By: Linus Ingvarsson <81238060+linke-droid@users.noreply.github.com>

--- GeoFlex/src/main/frontend/geoflex/src/shared/Codebox.js
+++ GeoFlex/src/main/frontend/geoflex/src/shared/Codebox.js
@@ -3,122 +3,120 @@ import axios from 'axios';
 
 export default class Codebox extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            number1: '',
-            number2: '',
-            number3: '',
-            number4: '',
-            status: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      number1: '',
+      number2: '',
+      number3: '',
+      number4: '',
+      status: ""
 
-        };
-        this.inputNumbers = []
-        this.inputs = {};
-        this.checkRoute = this.checkRoute.bind(this);
+    };
+    this.inputNumbers = []
+    this.inputs = {};
+    this.checkRoute = this.checkRoute.bind(this);
+  }
+
+  componentDidMount() {
+    const inputs = document.getElementById("inputs");
+    inputs.firstElementChild.focus();
+  }
+
+  handleChange = async event => {
+    console.log(event.target.value)
+    //const { name, value } = event.target;
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    const numbers = this.inputNumbers;
+
+    if (/^\d$/.test(value)) {
+
+      this.setState({ [name]: value });
+      numbers.push(value);
+      console.log("HÄR")
+      console.log(numbers)
+      const nextInputField = this.inputs[name].nextSibling;
+      if (nextInputField) {
+        nextInputField.focus();
+      } else {
+
+        this.checkRoute(numbers);
+      }
+    } else {
+      this.setState({ [name]: value.slice(0, 1) });
     }
+  }
 
-    componentDidMount() {
-        const inputs = document.getElementById("inputs");
-        inputs.firstElementChild.focus();
+  handleKeyDown = event => {
+    if ((event.key === 'Backspace' || event.key === 'Delete') && event.target.value === '') {
+      event.preventDefault();
+      const { name } = event.target;
+      const prevInputField = this.inputs[name].previousSibling;
+      if (prevInputField) {
+        this.setState({ [name]: '' });
+        prevInputField.focus();
+      }
     }
+  }
 
-    handleChange = async event => {
-        console.log(event.target.value)
-        //const { name, value } = event.target;
-        const target = event.target;
-        const name = target.name;
-        const value = target.value;
-        const numbers = this.inputNumbers;
+  checkRoute(numbers) {
+    console.log("CHECKROUTE")
+    let routeCode;
+    console.log(numbers.length)
+    if (numbers.length === 4) {
+      routeCode = numbers.join('');
+      const url = "/user/checkRoute?routeCode=" + routeCode
+      axios.get(url)
+        .then(response => {
+          if (response.data != "-1") {
+            let url = "/game/" + response.data + "/welcome"
+            this.props.handleNavigate(url, routeCode, response.data);
 
-        if (/^\d$/.test(value)) {
-
-            this.setState({ [name]: value });
-            numbers.push(value);
-            console.log("HÄR")
-            console.log(numbers)
-            const nextInputField = this.inputs[name].nextSibling;
-            if (nextInputField) {
-                nextInputField.focus();
-            } else {
-
-                this.checkRoute(numbers);
-            }
-        } else {
-            this.setState({ [name]: value.slice(0, 1) });
-        }
-    }
-
-    handleKeyDown = event => {
-        if ((event.key === 'Backspace' || event.key === 'Delete') && event.target.value === '') {
-            event.preventDefault();
-            const { name } = event.target;
-            const prevInputField = this.inputs[name].previousSibling;
-            if (prevInputField) {
-                this.setState({ [name]: '' });
-                prevInputField.focus();
-            }
-        }
-    }
-
-    checkRoute(numbers) {
-        console.log("CHECKROUTE")
-        let routeCode;
-        console.log(numbers.length)
-        if (numbers.length === 4) {
-            routeCode = numbers.join('');
-            const url = "/user/checkRoute?routeCode=" + routeCode
-            axios.get(url)
-                .then(response => {
-                    if (response.data != "-1") {
-                        let url = "/game/" + response.data + "/welcome"
-                        this.props.handleNavigate(url, routeCode, response.data );
-                        
-                        this.setState({ status: "Laddar quiz" })
-                    } else {
-                        this.setState({ status: "Fel kod, försök igen." })
-                        this.inputNumbers = []
-                        this.setState({
-                            number1: '',
-                            number2: '',
-                            number3: '',
-                            number4: ''
-                        })
-                        const inputs = document.getElementById("inputs");
-                        inputs.firstElementChild.focus();
-                    }
-                })
-                .catch(error => {
-                    console.log(error)
-                    /*
-                    let url = "/game/1234/welcome"
-                        this.props.handleNavigate(url, "3956", "2039" );
-                    */
-                });
-        } else {
+            this.setState({ status: "Laddar quiz" })
+          } else {
             this.setState({ status: "Fel kod, försök igen." })
             this.inputNumbers = []
             this.setState({
-                number1: '',
-                number2: '',
-                number3: '',
-                number4: ''
+              number1: '',
+              number2: '',
+              number3: '',
+              number4: ''
             })
             const inputs = document.getElementById("inputs");
             inputs.firstElementChild.focus();
-        }
+          }
+        })
+        .catch(error => {
+          console.log(error)
+          /*
+          let url = "/game/1234/welcome"
+              this.props.handleNavigate(url, "3956", "2039" );
+          */
+        });
+    } else {
+      this.setState({ status: "Fel kod, försök igen." })
+      this.inputNumbers = []
+      this.setState({
+        number1: '',
+        number2: '',
+        number3: '',
+        number4: ''
+      })
+      const inputs = document.getElementById("inputs");
+      inputs.firstElementChild.focus();
     }
+  }
 
   render() {
     return (<>
       <div className='row'>
-        <div className='col'>
+        <div className='col center-align'>
           <b>Skriv in din fyrsiffriga kod eller scanna QR-kod för att gå med i rundan.</b>
-          <p>{this.state.status}</p>
         </div>
       </div>
-
-      <div className='row'>
+      <div id="inputs" className='row'>
         <input
           type="text"
           name="number1"
@@ -149,15 +147,16 @@ export default class Codebox extends Component {
         <input
           type="text"
           name="number4"
-          className="col s1 white offset-s2"
+          className="col s1 offset-s2"
           ref={(input) => { this.inputs.number4 = input; }}
           value={this.state.number4}
           onChange={this.handleChange}
           onKeyDown={this.handleKeyDown} />
       </div>
-      <div className='row'>
-                <p>{this.state.status}</p>
-            </div>
+      <div className='row center-align red-text'>
+        {this.state.status}
+
+      </div>
     </>
     );
   }
