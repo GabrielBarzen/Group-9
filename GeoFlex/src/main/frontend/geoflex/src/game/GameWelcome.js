a586a23d807b51d2710c6242a5bec90295cccb2e
TobiasSvenssonDev <tobias_svensson84@live.se>
2023-01-11 23:45:32 +0100

    Update GameWelcome.js
    Co-Authored-By: Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>

--- GeoFlex/src/main/frontend/geoflex/src/game/GameWelcome.js
+++ GeoFlex/src/main/frontend/geoflex/src/game/GameWelcome.js
@@ -29,16 +29,16 @@ export default function GameWelcome(props) {
                     </div>
                   </div>
                   <div className='row'>
-                {/* Add a conditional statement to render the appropriate element based on the values of data.media[0].externalMedia and data.media[0].mediaType */}
-                {props.data.routeMedia.externalMedia && props.data.routeMedia.mediaType === "video" ? (
+                {/* Add a conditional statement to render the appropriate element based on the values of props.welcomeData.routeMedia.externalMedia and props.welcomeData.routeMedia.mediaType */}
+                {props.welcomeData.routeMedia.externalMedia && props.welcomeData.routeMedia.mediaType === "video" ? (
                   // Render an iframe element
-                  <iframe src={props.data.routeMedia.mediaURL} title="youtube video" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                ) : props.data.routeMedia.mediaType === "video" ? (
+                  <iframe src={props.welcomeData.routeMedia.mediaURL} title="youtube video" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                ) : props.welcomeData.routeMedia.mediaType === "video" ? (
                   // Render a video element
-                  <video src={props.data.routeMedia.mediaURL} controls></video>
+                  <video src={props.welcomeData.routeMedia.mediaURL} controls></video>
                 ) : (
                   // Render an img element
-                  <img src={props.data.routeMedia.mediaURL} alt="questionImage" className='responsive-img' style={{ "borderRadius": "5px" }}></img>
+                  <img src={props.welcomeData.routeMedia.mediaURL} alt="questionImage" className='responsive-img' style={{ "borderRadius": "5px" }}></img>
                 )}
               </div>
                   <div className='row'>
