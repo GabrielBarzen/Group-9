d33e1b232ee0b8e2fa3904c43424b2d4c3d5ca08
TobiasSvenssonDev <tobias_svensson84@live.se>
2023-01-11 23:43:00 +0100

    img welcome game
    Co-Authored-By: Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>

--- GeoFlex/src/main/frontend/geoflex/src/game/GameWelcome.js
+++ GeoFlex/src/main/frontend/geoflex/src/game/GameWelcome.js
@@ -29,12 +29,18 @@ export default function GameWelcome(props) {
                     </div>
                   </div>
                   <div className='row'>
-                    <div className='col s12'>
-                      <p>
-                          {props.welcomeData.image}
-                      </p>
-                    </div>
-                  </div>
+                {/* Add a conditional statement to render the appropriate element based on the values of data.media[0].externalMedia and data.media[0].mediaType */}
+                {props.data.routeMedia.externalMedia && props.data.routeMedia.mediaType === "video" ? (
+                  // Render an iframe element
+                  <iframe src={props.data.routeMedia.mediaURL} title="youtube video" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                ) : props.data.routeMedia.mediaType === "video" ? (
+                  // Render a video element
+                  <video src={props.data.routeMedia.mediaURL} controls></video>
+                ) : (
+                  // Render an img element
+                  <img src={props.data.routeMedia.mediaURL} alt="questionImage" className='responsive-img' style={{ "borderRadius": "5px" }}></img>
+                )}
+              </div>
                   <div className='row'>
                     <div className='col s12'>
                       <p>
