dffbaa7ce3ed2376ecfe4a99cbefc4867cee6ca3
Mahmoud Daabas <77792515+mahmouddaabas@users.noreply.github.com>
2023-01-13 15:12:39 +0100

    Update About.js

--- GeoFlex/src/main/frontend/geoflex/src/pages/About.js
+++ GeoFlex/src/main/frontend/geoflex/src/pages/About.js
@@ -6,11 +6,9 @@ export default function About() {
         <div className='row center-align'>
             <div className='container white container-css'>
                 <h5>Vad är GeoFlex?</h5>
-                <p>GeoFlex är en applikation som har utvecklats av en studentgrupp på Malmö Universitet.</p>
-            </div>
-            <div className='container white container-css'>
-                <h5>Vem är vi?</h5>
-                <p>Kontakt info:</p>
+                <p>GeoFlex är en applikation som har utvecklats av en studentgrupp på Malmö Universitet.
+                    <br></br>
+                    Syftet med appen är att kunna skapa quiz och info rundor som sen kan delas ut och spelas av användare.</p>
             </div>
             <div className='row center-align container'>
                 <Link className='btn col s12 center-align waves-teal btn col btn-large btn-css icon-css z-depth-2' to="/">
